82eb8ff5bc5c527fea8225ffcc5a833d994c632d
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2022-10-25 15:58:16 +0200

    feat(Refactor) : Rename base path and remove unused constant

--- src/main/java/it/finanze/sanita/fse2/ms/srvsemanticrulesmanager/config/Constants.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvsemanticrulesmanager/config/Constants.java
@@ -16,7 +16,7 @@ public final class Constants {
 		/**
 		 * Base path.
 		 */
-		public static final String BASE = "it.sanita.srvsemanticrulesmanager";
+		public static final String BASE = "it.finanze.sanita.fse2.ms.srvsemanticrulesmanager";
 
 		/**
 		 * Controller path.
@@ -26,30 +26,30 @@ public final class Constants {
 		/**
 		 * Service path.
 		 */
-		public static final String SERVICE = "it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.servic";
+		public static final String SERVICE = "it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.service";
 
 		/**
 		 * Configuration path.
 		 */
 		public static final String CONFIG = "it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.config";
-		
+
 		/**
 		 * Configuration mongo path.
 		 */
 		public static final String CONFIG_MONGO = "it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.config.mongo";
-		
+
 		/**
 		 * Configuration mongo repository path.
 		 */
 		public static final String REPOSITORY_MONGO = "it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.repository";	 
-		
-		
+
+
 		private ComponentScan() {
 			//This method is intentionally left blank.
 		}
 
 	}
-	
+
 	public static final class Collections {
 
 		/**
@@ -61,24 +61,12 @@ public final class Constants {
 
 		}
 	}
-	
-	public static final class Headers {
-		
-		/**
-		 * JWT header field.
-		 */
-		public static final String JWT_HEADER = "Authorization";
 
-		private Headers() {
-			//This method is intentionally left blank.
-		}
 
-	}
- 
 	public static final class Profile {
-		
+
 		public static final String TEST = "test"; 
-		
+
 		public static final String TEST_PREFIX = "test_"; 
 
 
@@ -92,87 +80,77 @@ public final class Constants {
 	}
 
 	public static final class App {
-		
+
 		public static final String JWT_TOKEN_AUDIENCE = "eds-semantic";
-
-		public static final String JWT_TOKEN_TYPE = "JWT";
-
-		public static final String BEARER_PREFIX = "Bearer "; 
 
 		public static final String BODY = "body";
 
 		public static final String MONGO_ID = "_id"; 
-		
+
 		public static final String FILENAME = "filename";
-		
+
 		public static final String VERSION = "version";
 
 		public static final String CONTENT_FILE = "content_file"; 
-		
+
 		public static final String CONTENT_SCHEMATRON = "content_schematron"; 
-		
+
 		public static final String LAST_UPDATE = "last_update_date"; 
-		
+
 		public static final String TEMPLATE_ID_ROOT = "template_id_root"; 
-		
+
 		public static final String DELETED = "deleted"; 
-		
-		public static final String SHA_256 = "SHA-256"; 
 
 
 		private App() {
 			//This method is intentionally left blank.
 		}
 
-		public static final class Collections {
-			public static final String SCHEMATRON = "schematron";
-			
-			private Collections() {
-			}
-		}
 	}
-	
+
 	public static final class Logs {
-		 
-		 public static final String ERROR_DELETE_SCHEMATRON = "Error while deleting ety schematron :"; 
-		 
-		 public static final String ERROR_RETRIEVING_HOST_INFO = "Error while retrieving host informations"; 
-		 
-		 public static final String ERROR_RETRIEVING_INSERTIONS = "Error retrieving insertions"; 
-		 
-		 public static final String ERROR_RETRIEVING_MODIFICATIONS = "Error retrieving modifications"; 
 
-		 public static final String ERROR_UNABLE_FIND_INSERTIONS = "Unable to retrieve change-set insertions"; 
-		 
-		 public static final String ERROR_UNABLE_FIND_MODIFICATIONS = "Unable to retrieve change-set modifications"; 
-		 
-		 public static final String ERROR_UNABLE_FIND_DELETIONS = "Unable to retrieve change-set deletions"; 
+		public static final String ERROR_DELETE_SCHEMATRON = "Error while deleting ety schematron :"; 
 
-		 public static final String ERROR_UNABLE_RETRIEVE_AVAILABLE_DICTIONARY = "Unable to retrieve every available dictionary"; 
-		 
-		 public static final String ERROR_EXECUTE_EXIST_VERSION_QUERY = "Error while execute exists by version query "; 
-		 
-		 public static final String ERROR_COMPUTING_SHA = "Errore in fase di calcolo SHA-256"; 
-		 
-		 public static final String ERROR_JSON_HANDLING = "Errore gestione json :"; 
-		 
-		 public static final String ERROR_EMPTY_DOCUMENT = "Error: inserted document is empty"; 
-		 
-		 public static final String ERROR_DOCUMENT_NOT_FOUND = "Error: document not found in collection"; 
-		 
-		 public static final String ERROR_REQUESTED_DOCUMENT_DOES_NOT_EXIST = "The requested document does not exists"; 
+		public static final String ERROR_RETRIEVING_HOST_INFO = "Error while retrieving host informations"; 
 
-		 public static final String ERROR_INVALID_OBJECT_ID = "The following string is not a valid object id: ";
-        public static final String ERROR_INSERT_SCHEMATRON = "Error inserting all etys";
+		public static final String ERROR_RETRIEVING_INSERTIONS = "Error retrieving insertions"; 
+
+		public static final String ERROR_RETRIEVING_MODIFICATIONS = "Error retrieving modifications"; 
+
+		public static final String ERROR_UNABLE_FIND_INSERTIONS = "Unable to retrieve change-set insertions"; 
+
+		public static final String ERROR_UNABLE_FIND_MODIFICATIONS = "Unable to retrieve change-set modifications"; 
+
+		public static final String ERROR_UNABLE_FIND_DELETIONS = "Unable to retrieve change-set deletions"; 
+
+		public static final String ERROR_UNABLE_RETRIEVE_AVAILABLE_DICTIONARY = "Unable to retrieve every available dictionary"; 
+
+		public static final String ERROR_EXECUTE_EXIST_VERSION_QUERY = "Error while execute exists by version query "; 
+
+		public static final String ERROR_COMPUTING_SHA = "Errore in fase di calcolo SHA-256"; 
+
+		public static final String ERROR_JSON_HANDLING = "Errore gestione json :"; 
+
+		public static final String ERROR_EMPTY_DOCUMENT = "Error: inserted document is empty"; 
+
+		public static final String ERROR_DOCUMENT_NOT_FOUND = "Error: document not found in collection"; 
+
+		public static final String ERROR_REQUESTED_DOCUMENT_DOES_NOT_EXIST = "The requested document does not exists"; 
+
+		public static final String ERROR_INVALID_OBJECT_ID = "The following string is not a valid object id: ";
+		
+		public static final String ERROR_INSERT_SCHEMATRON = "Error inserting all etys";
+		
 		public static final String ERROR_RETRIEVING_SCHEMATRON = "Error while retrieving schematron";
 
 
 		private Logs() {
-				//This method is intentionally left blank. 
-			}
+			//This method is intentionally left blank. 
+		}
 
 	}
-  
+
 	/**
 	 *	Constants.
 	 */
