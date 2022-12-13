a1bbab4683cc9b19aba70577fb1d7a1c48704e4b
Simone Le Noci <simonelenocidrum@gmail.com>
2022-12-13 15:06:48 +0100

    refactor : CrudDocumentResDTO

--- src/main/java/it/finanze/sanita/fse2/ms/srvsemanticrulesmanager/dto/response/impl/CrudDocumentResDTO.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvsemanticrulesmanager/dto/response/impl/CrudDocumentResDTO.java
@@ -22,18 +22,18 @@ public class CrudDocumentResDTO extends ResponseDTO {
 	 */
 	private static final long serialVersionUID = 5857196886068379718L;
  
-	private Integer insertedSchematron;
-	private Integer updatedSchematron;
-	private Integer deletedSchematron;
+	private Integer insertedItems;
+	private Integer updatedItems;
+	private Integer deletedItems;
 
 	public CrudDocumentResDTO() {
 		super();
 	}
 
-	public CrudDocumentResDTO(LogTraceInfoDTO traceInfo, Integer insertedSchematron, Integer updatedSchematron, Integer deletedSchematron) {
+	public CrudDocumentResDTO(LogTraceInfoDTO traceInfo, Integer insertedItems, Integer updatedItems, Integer deletedItems) {
 		super(traceInfo);
-		this.insertedSchematron = insertedSchematron;
-		this.updatedSchematron = updatedSchematron;
-		this.deletedSchematron = deletedSchematron;
+		this.insertedItems = insertedItems;
+		this.updatedItems = updatedItems;
+		this.deletedItems = deletedItems;
 	}
 }
