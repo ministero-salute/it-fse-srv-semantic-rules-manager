015eca3a2e7011e0e9c7bbbbb2468ba3d0ce8c02
Simone Le Noci <simonelenocidrum@gmail.com>
2022-12-15 17:18:28 +0100

    refactor : RouteUtility and ISchematronCTL

--- src/main/java/it/finanze/sanita/fse2/ms/srvsemanticrulesmanager/controller/ISchematronCTL.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvsemanticrulesmanager/controller/ISchematronCTL.java
@@ -3,6 +3,30 @@
  */
 package it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.controller;
 
+import static it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.utility.RouteUtility.*;
+import static it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.utility.ValidationUtility.DEFAULT_STRING_MAX_SIZE;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -17,33 +41,25 @@ import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.dto.response.crud.PutDo
 import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.dto.response.error.base.ErrorResponseDTO;
 import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.dto.response.impl.GetDocumentResDTO;
 import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.dto.response.impl.GetDocumentsResDTO;
-import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.exceptions.*;
+import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.exceptions.DocumentAlreadyPresentException;
+import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.exceptions.DocumentNotFoundException;
+import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.exceptions.EmptyDocumentException;
+import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.exceptions.InvalidContentException;
+import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.exceptions.InvalidVersionException;
+import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.exceptions.OperationException;
+import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.exceptions.SchematronValidatorException;
 import it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.validators.ValidObjectId;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-import java.io.IOException;
-import java.io.Serializable;
-
-import static it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.utility.RouteUtility.API_QP_INCLUDE_DELETED;
-import static it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.utility.ValidationUtility.DEFAULT_STRING_MAX_SIZE;
 
 /**
  * Schematron Controller.
  * 
  */
-@RequestMapping(path = "/v1")
-@Tag(name = "Schematron Controller")
+@RequestMapping(path = API_SCHEMATRON_MAPPER)
+@Tag(name = API_SCHEMATRONS_TAG)
 @Validated
 public interface ISchematronCTL extends Serializable {
 
-        @PostMapping(value = "/schematron", produces = {
+        @PostMapping(produces = {
                         MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
         @Operation(summary = "Add schematron to MongoDB", description = "Servizio che consente di aggiungere uno schematron alla base dati.")
         @ApiResponses(value = {
@@ -53,12 +69,12 @@ public interface ISchematronCTL extends Serializable {
                         @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ErrorResponseDTO.class))) })
         @ResponseStatus(HttpStatus.CREATED)
         PostDocsResDTO addSchematron(
-                        @RequestPart("templateIdRoot") @Parameter(description = "Template Id Root of the Schematron", schema = @Schema(minLength = 1, maxLength = 100)) @Size(min = 1, max = 100) @NotBlank(message = "Template Id cannot be blank") String templateIdRoot,
-                        @RequestPart("version") @Parameter(description = "Schematron version", schema = @Schema(minLength = 1, maxLength = 100)) @Size(min = 1, max = 100) @NotBlank(message = "version cannot be blank") @Pattern(message = "Version does not match the regex ^(\\d+\\.)(\\d+)$", regexp = "^(\\d+\\.)(\\d+)$") String version,
-                        @RequestPart("file") MultipartFile file)
+                        @RequestPart(API_PATH_TEMPLATEIDROOT_VAR) @Parameter(description = "Template Id Root of the Schematron", schema = @Schema(minLength = 1, maxLength = 100)) @Size(min = 1, max = 100) @NotBlank(message = "Template Id cannot be blank") String templateIdRoot,
+                        @RequestPart(API_PATH_VERSION_VAR) @Parameter(description = "Schematron version", schema = @Schema(minLength = 1, maxLength = 100)) @Size(min = 1, max = 100) @NotBlank(message = "version cannot be blank") @Pattern(message = "Version does not match the regex ^(\\d+\\.)(\\d+)$", regexp = "^(\\d+\\.)(\\d+)$") String version,
+                        @RequestPart(API_PATH_FILE_VAR) MultipartFile file)
             throws IOException, OperationException, EmptyDocumentException, DocumentAlreadyPresentException, DocumentNotFoundException, InvalidContentException, SchematronValidatorException;
 
-        @PutMapping(value = "/schematron", produces = { MediaType.APPLICATION_JSON_VALUE }, consumes = {
+        @PutMapping(produces = { MediaType.APPLICATION_JSON_VALUE }, consumes = {
                         MediaType.MULTIPART_FORM_DATA_VALUE })
         @Operation(summary = "Update schematron on MongoDB", description = "Servizio che consente di aggiornare uno schematron sulla base dati.")
         @ApiResponses(value = {
@@ -68,13 +84,13 @@ public interface ISchematronCTL extends Serializable {
                         @ApiResponse(responseCode = "409", description = "Conflitto riscontrato sulla risorsa", content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ErrorResponseDTO.class))),
                         @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ErrorResponseDTO.class))) })
         PutDocsResDTO updateSchematron(
-                        @RequestPart("templateIdRoot") @Parameter(description = "Template Id Root of the Schematron", schema = @Schema(minLength = 1, maxLength = 100)) @Size(min = 1, max = 100) @NotBlank(message = "Template Id cannot be blank") String templateIdRoot,
-                        @RequestPart("version") @Parameter(description = "Schematron version", schema = @Schema(minLength = 1, maxLength = 100)) @Size(min = 1, max = 100) @NotBlank(message = "Version cannot be blank") @Pattern(message = "Version does not match the regex ^(\\d+\\.)(\\d+)$", regexp = "^(\\d+\\.)(\\d+)$") String version,
-                        @RequestPart("file") MultipartFile file)
+                        @RequestPart(API_PATH_TEMPLATEIDROOT_VAR) @Parameter(description = "Template Id Root of the Schematron", schema = @Schema(minLength = 1, maxLength = 100)) @Size(min = 1, max = 100) @NotBlank(message = "Template Id cannot be blank") String templateIdRoot,
+                        @RequestPart(API_PATH_VERSION_VAR) @Parameter(description = "Schematron version", schema = @Schema(minLength = 1, maxLength = 100)) @Size(min = 1, max = 100) @NotBlank(message = "Version cannot be blank") @Pattern(message = "Version does not match the regex ^(\\d+\\.)(\\d+)$", regexp = "^(\\d+\\.)(\\d+)$") String version,
+                        @RequestPart(API_PATH_FILE_VAR) MultipartFile file)
             throws IOException, OperationException, DocumentNotFoundException, InvalidContentException, InvalidVersionException, SchematronValidatorException,
             DocumentAlreadyPresentException;
 
-        @DeleteMapping(value = "/schematron/{templateIdRoot}", produces = {MediaType.APPLICATION_JSON_VALUE })
+        @DeleteMapping(value = API_DELETE_BY_TEMPLATEIDROOT, produces = {MediaType.APPLICATION_JSON_VALUE })
         @Operation(summary = "Delete schematron from MongoDB given its Template ID Root", description = "Servizio che consente di cancellare uno schematron dalla base dati dato il Template ID Root")
         @ApiResponses(value = {
                         @ApiResponse(responseCode = "200", description = "Cancellazione Schematron avvenuta con successo", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DelDocsResDTO.class))),
@@ -87,7 +103,7 @@ public interface ISchematronCTL extends Serializable {
             String templateIdRoot
         ) throws DocumentNotFoundException, OperationException;
 
-        @GetMapping(value = "/schematron/{templateIdRoot}", produces = {MediaType.APPLICATION_JSON_VALUE })
+        @GetMapping(value = API_GET_BY_TEMPLATEIDROOT, produces = {MediaType.APPLICATION_JSON_VALUE })
         @Operation(summary = "Returns a schematron from MongoDB, given its Template ID Root", description = "Servizio che consente di ritornare uno schematron dalla base dati dati il suo Template ID Root e Version.")
         @ApiResponses(value = {
                 @ApiResponse(responseCode = "200", description = "Richiesta Schematron avvenuta con successo", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = GetDocumentsResDTO.class))),
@@ -100,11 +116,11 @@ public interface ISchematronCTL extends Serializable {
             @PathVariable
             @Size(max = DEFAULT_STRING_MAX_SIZE, message = "templateIdRoot does not match the expected size")
             String templateIdRoot,
-            @RequestParam(value = "includeBinary", defaultValue = "false") @Parameter(description = "Include binary content") boolean binary,
+            @RequestParam(value = API_QP_INCLUDE_BINARY, defaultValue = "false") @Parameter(description = "Include binary content") boolean binary,
             @RequestParam(value = API_QP_INCLUDE_DELETED, defaultValue = "false") @Parameter(description = "Include deleted content") boolean deleted
         ) throws DocumentNotFoundException, OperationException;
 
-        @GetMapping(value = "/schematron/id/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
+        @GetMapping(value = API_GET_ONE_BY_ID, produces = { MediaType.APPLICATION_JSON_VALUE })
         @Operation(summary = "Returns a Schematron from MongoDB, given its ID", description = "Servizio che consente di ritornare uno Schematron dalla base dati dati il suo ID.")
         @ApiResponses(value = {
                         @ApiResponse(responseCode = "200", description = "Richiesta Schematron avvenuta con successo", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = SchematronDocumentDTO.class))),
@@ -113,13 +129,13 @@ public interface ISchematronCTL extends Serializable {
                         @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ErrorResponseDTO.class))) })
         GetDocumentResDTO getSchematronById(@NotBlank(message = "Id cannot be blank") @PathVariable @Size(max = DEFAULT_STRING_MAX_SIZE, message = "id does not match the expected size") @ValidObjectId(message = "Document id not valid") String id) throws OperationException, DocumentNotFoundException;
 
-        @GetMapping(value = "/schematron", produces = {MediaType.APPLICATION_JSON_VALUE })
+        @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE })
         @Operation(summary = "Returns the list of all schematrons from MongoDB", description = "Servizio che consente di ritornare la lista degli schematron dalla base dati.")
         @ApiResponses(value = {
                         @ApiResponse(responseCode = "200", description = "Richiesta Schematron avvenuta con successo", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = GetDocumentsResDTO.class))),
                         @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ErrorResponseDTO.class))) })
         GetDocumentsResDTO getSchematrons(
-            @RequestParam(value = "includeBinary", defaultValue = "false") @Parameter(description = "Include binary content") boolean binary,
+            @RequestParam(value = API_QP_INCLUDE_BINARY, defaultValue = "false") @Parameter(description = "Include binary content") boolean binary,
             @RequestParam(value = API_QP_INCLUDE_DELETED, defaultValue = "false") @Parameter(description = "Include deleted content") boolean deleted
         ) throws OperationException;
 
--- src/main/java/it/finanze/sanita/fse2/ms/srvsemanticrulesmanager/utility/RouteUtility.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvsemanticrulesmanager/utility/RouteUtility.java
@@ -1,7 +1,30 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 package it.finanze.sanita.fse2.ms.srvsemanticrulesmanager.utility;
 
 public class RouteUtility {
+	
+    /**
+     * Private constructor to disallow to access from other classes
+     */
+    private RouteUtility() {}
 
+	public static final String API_VERSION = "v1";
+	public static final String API_SCHEMATRON = "schematron";
+	public static final String API_ID = "id";
+	public static final String API_SCHEMATRON_MAPPER = "/" + API_VERSION + "/" + API_SCHEMATRON;
+	
     public static final String API_QP_INCLUDE_DELETED = "includeDeleted";
-
+    public static final String API_QP_INCLUDE_BINARY = "includeBinary";
+    public static final String API_PATH_ID_VAR = "id";
+    public static final String API_PATH_TEMPLATEIDROOT_VAR = "templateIdRoot";
+    public static final String API_PATH_VERSION_VAR = "version";
+    public static final String API_PATH_FILE_VAR = "file";
+    public static final String API_PATH_TEMPLATEIDROOT = "/{" + API_PATH_TEMPLATEIDROOT_VAR + "}";
+    public static final String API_ID_EXTS = "/{" + API_PATH_ID_VAR + "}";
+    public static final String API_GET_ONE_BY_ID = API_ID + API_ID_EXTS;
+    public static final String API_DELETE_BY_TEMPLATEIDROOT = API_PATH_TEMPLATEIDROOT;
+    public static final String API_GET_BY_TEMPLATEIDROOT = API_PATH_TEMPLATEIDROOT;
+    public static final String API_SCHEMATRONS_TAG = "Schematron Controller";
 }
